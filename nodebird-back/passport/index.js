d3c67128299b15ac0ebb8032f60db5e21098bc6b
ncb6206 <ncb6206@gmail.com>
2023-05-08 16:32:05 +0900

    백엔드 수정중

--- nodebird-back/passport/index.js
+++ nodebird-back/passport/index.js
@@ -4,6 +4,7 @@ const { User } = require("../models");
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
+    // 서버쪽에 [{id:1, cookie: 'clhxy'}]
     done(null, user.id);
   });
 
@@ -19,3 +20,11 @@ module.exports = () => {
 
   local();
 };
+
+// 프론트에서 서버로는 cookie만 보낸다(clhxy)
+// 서버가 쿠키파서, 익스프레서 세션으로 쿠키 검사 후 id : 1 발견
+// id: 1이 deserializeUser에 들어감
+// req.user로 사용자 정보가 들어감
+
+// 요청 보낼때마다 deserializeUser가 실행됨(db 요청 1번씩 실행)
+// 실무에서는 deserializeUser 결과물 캐싱
--- nodebird-back/routes/posts.js
+++ nodebird-back/routes/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Op } = require("sequelize");
 
 const { Post, Image, User, Comment } = require("../models");
 
@@ -7,7 +8,13 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   // GET /posts
   try {
+    const where = {};
+    if (parseInt(req.query.lastId, 10)) {
+      // 초기 로딩이 아닐 때
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) };
+    } // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
     const posts = await Post.findAll({
+      where,
       limit: 10,
       order: [
         ["createdAt", "DESC"],
